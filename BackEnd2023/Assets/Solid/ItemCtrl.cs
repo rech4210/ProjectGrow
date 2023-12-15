b85a71f40e8c5d6d5af7308b4cc82c19ef74eb4a
SolidTooth <34057217+SolidTooth@users.noreply.github.com>
2023-12-16 00:09:48 +0900

    no message

--- BackEnd2023/Assets/Solid/ItemCtrl.cs
+++ BackEnd2023/Assets/Solid/ItemCtrl.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ItemCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    ///아이템 태그
+    ///사용 함수 (루트 컨트롤 보내주세요
+    ///들기 놓기
+    public void UseCall(I_RootCtrl rootCtrl)
+    {//마우스 클릭의 아이템 사용 처리
+
+    }
+    public void GrabToggle(I_RootCtrl rootCtrl, bool isGrab)
+    {//들기 놓기 처리
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
