1528c5786cbc3c8f4e7384aaa7228d3d6aa972a7
SolidTooth <34057217+SolidTooth@users.noreply.github.com>
2023-12-16 00:31:54 +0900

    no message

--- BackEnd2023/Assets/Solid/ItemCtrl.cs
+++ BackEnd2023/Assets/Solid/ItemCtrl.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class ItemCtrl : MonoBehaviour
+public abstract class ItemCtrl : MonoBehaviour
 {
     ///아이템 태그
     ///사용 함수 (루트 컨트롤 보내주세요
     ///들기 놓기
-    public void UseCall(I_RootCtrl rootCtrl)
-    {//마우스 클릭의 아이템 사용 처리
+    ///
 
-    }
-    public void GrabToggle(I_RootCtrl rootCtrl, bool isGrab)
-    {//들기 놓기 처리
 
-    }
+    //마우스 클릭의 아이템 사용 처리
+    public abstract void UseCall(I_RootCtrl rootCtrl);
+    //들기 놓기 처리
+    public abstract void GrabToggle(I_RootCtrl rootCtrl, bool isGrab);
 
 }
---
+++ BackEnd2023/Assets/Solid/Item_Pot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Pot : ItemCtrl
+{
+    public override void GrabToggle(I_RootCtrl rootCtrl, bool isGrab)
+    {
+    }
+
+    public override void UseCall(I_RootCtrl rootCtrl)
+    {
+    }
+
+}
