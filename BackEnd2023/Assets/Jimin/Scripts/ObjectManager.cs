1d27508c1dd3bf8da027da1ad6c3ee3311834b5e
Jimin Lee <jiminlee@Jiminui-MacBookPro.local>
2023-12-16 04:19:16 +0900

    [ADD]ObjectPool

---
+++ BackEnd2023/Assets/Jimin/Scripts/ObjectManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class ObjectManager : MonoBehaviour
+{
+    [SerializeField]
+    public Zombies zombies;
+
+    //[SerializeField]
+    //public items items;
+
+    Vector2[] RandomVector = new Vector2[] { new Vector2(2f, 3f), new Vector2(5f, 5f) };
+
+
+    ObjectPooling<Zombies> zombiePool = new ObjectPooling<Zombies>();
+    //ObjectPooling<items> itemPool = new ObjectPooling<items>;
+
+    private void Awake()
+    {
+        zombiePool.Initialize(zombies, this.gameObject.transform, 10);
+        //itemPool.Initalize(items, this.gameObject.transform, 10);
+    }
+
+    protected void MakeZombie()
+    {
+        Zombies newzombie = zombiePool.GetObject(zombies);
+        newzombie.transform.position = RandomVector[Random.Range(0, RandomVector.Length)];
+    }
+}
