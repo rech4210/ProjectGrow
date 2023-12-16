53f8ebe2872510ed4466205b272f03b6a270b7d3
SolidTooth <34057217+SolidTooth@users.noreply.github.com>
2023-12-16 11:26:38 +0900

    no message

--- BackEnd2023/Assets/Solid/BulletCtrl.cs
+++ BackEnd2023/Assets/Solid/BulletCtrl.cs
@@ -1,10 +1,27 @@
 ﻿
 using UnityEngine;
 using System.Collections;
+using System;
 
-public class BulletCtrl : MonoBehaviour
+public class BulletCtrl : MonoBehaviour, I_Pool
 {
+    public float radius = 0.1f;
     public Vector2 dic;
     public float range;
     public float speed;
+    public Action<I_Pool> disalbleAction;
+    public void SetPoolEvent(Action<I_Pool> poolevent)
+    {
+        disalbleAction += poolevent;
+    }
+    public void disable()
+    {
+        disalbleAction?.Invoke(this);
+    }
+    private void Update()
+    {
+        RaycastHit[] hits;
+        float moveRange = Time.deltaTime * speed;
+        Physics2D.CircleCastAll(this.transform.position, radius, dic, moveRange);
+    }
 }
