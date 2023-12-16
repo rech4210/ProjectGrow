1f8ffeafa5ec68894f26ff6691974dadadead04b
Jimin Lee <jiminlee@Jiminui-MacBookPro.local>
2023-12-17 03:25:02 +0900

    [ADD] lighting

--- BackEnd2023/Assets/Jimin/Scripts/LightingEffect.cs
+++ BackEnd2023/Assets/Jimin/Scripts/LightingEffect.cs
@@ -18,6 +18,8 @@ public class LightingEffect : MonoBehaviour
     bool bisTurnOn;
     List<Vector2> monstertransform = new List<Vector2>();
 
+    List<GameObject> Circlelist = new List<GameObject>();
+
     private void Start()
     {
         AttackLighting(tCircle);
@@ -31,16 +33,49 @@ public class LightingEffect : MonoBehaviour
         }
 
         LineRenderer line = Instantiate(Lighting);
-        Instantiate(LightCircle, this.gameObject.transform);
+        line.SetWidth(0, 0);
+
+        var tmp = Instantiate(LightCircle, this.gameObject.transform);
+       //tmp.SetActive(false);
+        Circlelist.Add(tmp);
+
         line.positionCount = monstertransform.Count + 1;
         line.SetPosition(0, this.gameObject.transform.position);
 
-
         for (int i = 0; i < monstertransform.Count; i++)
         {
-            Instantiate(LightCircle, transforms[i]);
+            tmp = Instantiate(LightCircle, transforms[i]);
+            tmp.SetActive(false);
+            Circlelist.Add(tmp);
             line.SetPosition(i + 1, monstertransform[i]);
         }
+
+        StartCoroutine(LightingCoroutine(line));
+    }
+
+    IEnumerator LightingCoroutine(LineRenderer line)
+    {
+        for (float i = 0; i < 0.05f; i += Time.deltaTime)
+        {
+            line.SetWidth(i, i);
+            foreach(var cir in Circlelist)
+            {
+                Color tmp = cir.GetComponent<SpriteRenderer>().color;
+                tmp.a += i;
+                cir.GetComponent<SpriteRenderer>().color = tmp;
+            }
+            yield return null;
+        }
+
+        //foreach (var i in Circlelist) i.SetActive(true);
+
+        for (float i = 1; i > 0f; i -= Time.deltaTime)
+        {
+            line.SetWidth(i, i);
+            yield return null;
+        }
+
+        //foreach (var i in Circlelist) i.SetActive(false);
     }
 
     public void TurnOn()
